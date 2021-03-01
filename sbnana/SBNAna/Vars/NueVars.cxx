6a6f32f3cbdafcce36ab5ae0a778dc48b77ef8d1
Edward Tyley <e.tyley@sheffield.ac.uk>
2021-03-01 04:31:16 -0600

    Update cuts for refactored SRPFP

--- sbnana/SBNAna/Vars/NueVars.cxx
+++ sbnana/SBNAna/Vars/NueVars.cxx
@@ -12,7 +12,7 @@ const Var kLargestRecoShowerIdx(
 
       for (unsigned int i = 0; i < slc->reco.nshw; i++) {
         auto const& shw = slc->reco.shw[i];
-        if (!shw.parent_is_primary)
+        if (!shw.pfp.parent_is_primary)
           continue;
         if (shw.bestplane_energy > maxEnergy) {
           bestIdx = i;
@@ -233,7 +233,7 @@ const Var kLongestTrackIdx(
 
       for (unsigned int i = 0; i < slc->reco.ntrk; i++) {
         auto const& trk = slc->reco.trk[i];
-        if (!trk.parent_is_primary)
+        if (!trk.pfp.parent_is_primary)
           continue;
 
         if (trk.len > maxLength) {
