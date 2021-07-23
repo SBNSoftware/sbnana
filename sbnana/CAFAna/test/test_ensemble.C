a20a913ce993a63ee2acb2a8200a071452cff6f9
Christopher Backhouse <bckhouse@fnal.gov>
2021-07-23 08:53:58 -0500

    Save/load spectra in test macro for faster iteration on plotting.

--- sbnana/CAFAna/test/test_ensemble.C
+++ sbnana/CAFAna/test/test_ensemble.C
@@ -1,10 +1,11 @@
 // Recommend running with a large --stride argument to just use the first file
 // in the dataset
 
+#include "sbnana/CAFAna/Core/Binning.h"
 #include "sbnana/CAFAna/Core/SpectrumLoader.h"
 #include "sbnana/CAFAna/Core/EnsembleRatio.h"
 #include "sbnana/CAFAna/Core/EnsembleSpectrum.h"
-#include "sbnana/CAFAna/Core/Binning.h"
+#include "sbnana/CAFAna/Core/LoadFromFile.h"
 #include "sbnana/CAFAna/Core/Var.h"
 
 #include "sbnana/CAFAna/Cuts/TruthCuts.h"
@@ -19,6 +20,7 @@
 #include "sbnana/SBNAna/Cuts/Cuts.h"
 
 #include "TCanvas.h"
+#include "TFile.h"
 #include "TH1.h"
 #include "TPad.h"
 
@@ -27,47 +29,59 @@ using namespace ana;
 // Since I'm in the SBND group the SAM dataset works fine for me
 const std::string sbnd_wildcard = "workshop_SBNWorkshop0421_prodoverlay_corsika_cosmics_proton_genie_nu_spill_gsimple-configf-v1_tpc_flat_caf_sbnd";
 
-void test_ensemble()
+const std::string state_fname = "test_ensemble_state.root";
+
+void test_ensemble(bool reload = false)
 {
-  SpectrumLoader loader(sbnd_wildcard);
+  if(reload || TFile(state_fname.c_str()).IsZombie()){
+    SpectrumLoader loader(sbnd_wildcard);
 
-  const Var kTrueE = SIMPLEVAR(truth.E);
+    const Var kTrueE = SIMPLEVAR(truth.E);
 
-  const Cut kNumuSel = kSlcNuScoreCut && kInFV && kSlcFlashMatchTimeCut && kSlcFlashMatchScoreCut;
+    const Cut kNumuSel = kSlcNuScoreCut && kInFV && kSlcFlashMatchTimeCut && kSlcFlashMatchScoreCut;
 
-  const Binning binsEnergy = Binning::Simple(30, 0, 3);
+    const Binning binsEnergy = Binning::Simple(30, 0, 3);
 
-  const HistAxis axEnergy("True energy (GeV)", binsEnergy, kTrueE);
+    const HistAxis axEnergy("True energy (GeV)", binsEnergy, kTrueE);
 
-  std::vector<Var> weis;
-  weis.reserve(100);
+    std::vector<Var> weis;
+    weis.reserve(100);
 
-  // We need these essentially as the list of knob names
-  const std::vector<const ISyst*>& systs = GetSBNGenieWeightSysts();
-  for(int i = 0; i < 100; ++i) weis.push_back(GetUniverseWeight(systs, i));
+    // We need these essentially as the list of knob names
+    const std::vector<const ISyst*>& systs = GetSBNGenieWeightSysts();
+    for(int i = 0; i < 100; ++i) weis.push_back(GetUniverseWeight(systs, i));
 
-  EnsembleSpectrum sCC(loader, axEnergy, kNoSpillCut, kNumuSel && kIsNumuCC, weis);
-  EnsembleSpectrum sNC(loader, axEnergy, kNoSpillCut, kNumuSel && kIsNC, weis);
+    EnsembleSpectrum sCC(loader, axEnergy, kNoSpillCut, kNumuSel && kIsNumuCC, weis);
+    EnsembleSpectrum sNC(loader, axEnergy, kNoSpillCut, kNumuSel && kIsNC, weis);
 
-  loader.Go();
+    loader.Go();
 
-  sCC.Nominal().ToTH1(kPOTnominal)->Draw("hist");
-  sNC.Nominal().ToTH1(kPOTnominal, kBlue)->Draw("hist same");
+    TFile fout(state_fname.c_str(), "RECREATE");
+    sCC.SaveTo(fout.mkdir("cc"));
+    sNC.SaveTo(fout.mkdir("nc"));
+  }
 
-  for(unsigned int i = 0; i < sCC.NUniverses(); ++i){
-    sCC.Universe(i).ToTH1(kPOTnominal, kGray)->Draw("hist same");
-    sNC.Universe(i).ToTH1(kPOTnominal, kBlue-10)->Draw("hist same");
+  TFile fin(state_fname.c_str());
+  EnsembleSpectrum* sCC = LoadFrom<EnsembleSpectrum>(fin.GetDirectory("cc")).release();
+  EnsembleSpectrum* sNC = LoadFrom<EnsembleSpectrum>(fin.GetDirectory("nc")).release();
+
+  sCC->Nominal().ToTH1(kPOTnominal, kRed)->Draw("hist");
+  sNC->Nominal().ToTH1(kPOTnominal, kBlue)->Draw("hist same");
+
+  for(unsigned int i = 0; i < sCC->NUniverses(); ++i){
+    sCC->Universe(i).ToTH1(kPOTnominal, kRed-10)->Draw("hist same");
+    sNC->Universe(i).ToTH1(kPOTnominal, kBlue-10)->Draw("hist same");
   }
 
   // Redraw the nominals over the top
-  sCC.Nominal().ToTH1(kPOTnominal)->Draw("hist same");
-  sNC.Nominal().ToTH1(kPOTnominal, kBlue)->Draw("hist same");
+  sCC->Nominal().ToTH1(kPOTnominal, kRed)->Draw("hist same");
+  sNC->Nominal().ToTH1(kPOTnominal, kBlue)->Draw("hist same");
 
   gPad->Print("test_ensemble.pdf");
 
   new TCanvas;
 
-  EnsembleRatio ratio = sCC/sNC;
+  EnsembleRatio ratio = *sCC / *sNC;
   TH1* hratio = ratio.Nominal().ToTH1();
   hratio->Draw("hist ][");
   hratio->GetYaxis()->SetTitle("CC / NC ratio");
