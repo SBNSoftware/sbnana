697ce7e5073eae07c159206eb3026f5d54ed0b7a
Ryan Howell <rhowell@icarusgpvm02.fnal.gov>
2022-09-20 11:43:50 -0500

    final changes

--- sbnana/CAFAna/Core/SpectrumLoader.cxx
+++ sbnana/CAFAna/Core/SpectrumLoader.cxx
@@ -211,17 +211,12 @@ namespace ana
 
     //    FloatingExceptionOnNaN fpnan;
 
-    long Nentries = tr->GetEntries();
-    if (max_entries != 0 && max_entries < Nentries) Nentries = max_entries;
-
     tr->LoadTree(N); // Only want a specific event from the TTree
 
     // If there is no husk field there is no concept of husk events
     if(!has_husk) sr.hdr.husk = false;
 
     HandleRecord(&sr);
-
-    if(prog) prog->SetProgress(double(N)/Nentries);
   }
 
   //----------------------------------------------------------------------
