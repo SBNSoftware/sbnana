336b8477e16a85ba65f3afa068d26c1f0d746e04
gputnam <gputnam@icarusbuild02.fnal.gov>
2023-09-01 14:45:25 -0500

    Update framework and nb code.

---
+++ sbnana/SBNAna/icarus-analysis-villiage/pyana/dimuon-tools/configs/evtdf.py
@@ -0,0 +1,2 @@
+DFS = [make_evtdf, make_hdrdf, make_mcnudf, make_mchdf]
+NAMES = ["evt", "hdr", "mcnu", "mch"]
--- sbnana/SBNAna/icarus-analysis-villiage/pyana/pyanalib/variable.py
+++ sbnana/SBNAna/icarus-analysis-villiage/pyana/pyanalib/variable.py
@@ -1,62 +1,61 @@
+def _extract_othr_value(othr, x):
+    if callable(othr):
+        return othr(x)
+    else:
+        return othr
+
 class Variable(object):
     def __init__(self, f):
         self.f = f
 
     def __and__(self, othr):
-        return Variable(lambda x: self.f(x) & othr.f(x))
+        return Variable(lambda x: self(x) & _extract_othr_value(othr, x))
 
     def __or__(self, othr):
-        return Variable(lambda x: self.f(x) | othr.f(x))
+        return Variable(lambda x: self(x) | _extract_othr_value(othr, x))
 
     def __matmul__(self, othr):
-        return Variable(lambda *args, **kw: self.f(othr.f(*args, **kw)))
+        return Variable(lambda *args, **kw: self(othr(*args, **kw)))
+
+    def __rmatmul__(self, othr):
+        return Variable(lambda *args, **kw: othr(self(*args, **kw)))
 
     def __invert__(self):
-        return Variable(lambda *args, **kw: ~self.f(*args, **kw))
+        return Variable(lambda *args, **kw: ~self(*args, **kw))
 
     def __call__(self, *args, **kw):
         return self.f(*args, **kw)
 
     def __lt__(self, othr):
-        if isinstance(othr, Variable):
-            return Variable(lambda x: self.f(x) < othr.f(x))
-        else:
-            return Variable(lambda x: self.f(x) < othr)
+        return Variable(lambda x: self.f(x) < _extract_othr_value(othr, x))
 
     def __le__(self, othr):
-        if isinstance(othr, Variable):
-            return Variable(lambda x: self.f(x) <= othr.f(x))
-        else:
-            return Variable(lambda x: self.f(x) <= othr)
+        return Variable(lambda x: self.f(x) <= _extract_othr_value(othr, x))
 
     def __gt__(self, othr):
-        if isinstance(othr, Variable):
-            return Variable(lambda x: self.f(x) > othr.f(x))
-        else:
-            return Variable(lambda x: self.f(x) > othr)
+        return Variable(lambda x: self.f(x) > _extract_othr_value(othr, x))
 
     def __ge__(self, othr):
-        if isinstance(othr, Variable):
-            return Variable(lambda x: self.f(x) >= othr.f(x))
-        else:
-            return Variable(lambda x: self.f(x) >= othr)
+        return Variable(lambda x: self.f(x) >= _extract_othr_value(othr, x))
+
     def __eq__(self, othr):
-        if isinstance(othr, Variable):
-            return Variable(lambda x: self.f(x) == othr.f(x))
-        else:
-            return Variable(lambda x: self.f(x) == othr)
+        return Variable(lambda x: self.f(x) == _extract_othr_value(othr, x))
 
     def __getattr__(self, idx):
         return Variable(lambda x: self.f(x).__getattr__(idx))
 
     def __sub__(self, othr):
-        return Variable(lambda x: self.f(x) - othr.f(x))
+        return Variable(lambda x: self.f(x) - _extract_othr_value(othr, x))
     def __add__(self, othr):
-        return Variable(lambda x: self.f(x) + othr.f(x))
+        return Variable(lambda x: self.f(x) + _extract_othr_value(othr, x))
     def __mul__(self, othr):
-        return Variable(lambda x: self.f(x) * othr.f(x))
+        return Variable(lambda x: self.f(x) * _extract_othr_value(othr, x))
     def __div__(self, othr):
-        return Variable(lambda x: self.f(x) / othr.f(x))
+        return Variable(lambda x: self.f(x) / _extract_othr_value(othr, x))
+    def __truediv__(self, othr):
+        return Variable(lambda x: self.f(x) / _extract_othr_value(othr, x))
+    def __floordiv__(self, othr):
+        return Variable(lambda x: self.f(x) // _extract_othr_value(othr, x))
 
 def VAR(f):
     return Variable(f)
