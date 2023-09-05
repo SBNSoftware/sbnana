87ef87bcdfc56efb21f00a87d00b6b218de7f55d
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2023-09-05 11:46:12 -0500

    Emptying NuMIXSecTrueEventVars

--- sbnana/SBNAna/Cuts/NuMIXSecTrueEventVars.h
+++ sbnana/SBNAna/Cuts/NuMIXSecTrueEventVars.h
@@ -8,43 +8,4 @@
 
 namespace ana{
 
-  // Weight
-  std::vector<double> GetNuMIPPFXWeightVectorPerNu(
-    const caf::SRSpillProxy* sr,
-    std::function<bool(const caf::SRTrueInteractionProxy&)> isSignal
-  );
-  const SpillMultiVar kNuMIPPFXWeightVectorPerSignalNu(
-    [](const caf::SRSpillProxy *sr) -> std::vector<double> {
-      return GetNuMIPPFXWeightVectorPerNu(sr, Is1muNp0piWithPhaseSpaceCut);
-    }
-  );
-
-  std::vector<double> GetSigmaWeightVectorPerNu(
-    const caf::SRSpillProxy* sr,
-    std::function<bool(const caf::SRTrueInteractionProxy&)> isSignal,
-    const std::string& psetName,
-    double shift // in sigma
-  );
-  SpillMultiVar GetSigmaWeightSpillMultiVarPerSignalNu(
-    const std::string& psetName,
-    double shift // in sigma
-  );
-
-  struct Univs
-  {
-    int i0, i1;
-    double w0, w1;
-  };
-
-  std::vector<double> GetUniverseWeightVectorPerNu(
-    const caf::SRSpillProxy* sr,
-    std::function<bool(const caf::SRTrueInteractionProxy&)> isSignal,
-    const std::string& psetName,
-    int univIdx
-  );
-  SpillMultiVar GetUniverseWeightSpillMultiVarPerSignalNu(
-    const std::string& psetName,
-    int univIdx
-  );
-
 }
