cd01e44a6da5b93fbb09e6a179b6a4f62ded1a72
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2023-09-15 11:56:55 -0500

    Adding EnsembleSpectrum with TruthVar

--- sbnana/CAFAna/Core/EnsembleSpectrum.h
+++ sbnana/CAFAna/Core/EnsembleSpectrum.h
@@ -30,6 +30,15 @@ namespace ana
                      const std::vector<Var>& univ_weis,
                      const Var& cv_wei = kUnweighted);
 
+    // TruthVar; but ordering the arguments similar to Spectrum
+    EnsembleSpectrum(const std::string& label, const Binning& bins,
+                     SpectrumLoaderBase& loader,
+                     const TruthVar& var,
+                     const TruthCut& truthcut,
+                     const SpillCut& spillcut,
+                     const std::vector<TruthVar>& univ_weis,
+                     const TruthVar& cv_wei = kTruthUnweighted);
+
     Spectrum Nominal() const {return fNom;}
     unsigned int NUniverses() const {return fUnivs.size();}
     Spectrum Universe(unsigned int i) const
