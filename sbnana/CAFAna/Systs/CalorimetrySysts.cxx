e94db688905220e3ecdc8b69329ebec86f009010
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2023-09-18 22:03:56 -0500

    Update detector paramters for calo syst

--- sbnana/CAFAna/Systs/CalorimetrySysts.cxx
+++ sbnana/CAFAna/Systs/CalorimetrySysts.cxx
@@ -6,14 +6,19 @@ namespace ana {
 
    CalorimetrySyst::CalorimetrySyst(CaloSyst _GainSyst, CaloSyst _AlphaSyst, CaloSyst _BetaSyst, const std::string& name, const std::string& latexName):
      ISyst(name, latexName),
+     temperature(8.75e1),
+     rho(-0.00615 * temperature + 1.928),
+     Efield(4.938e-1),
      gain(0.01265), alpha(0.93), beta(0.212),
-     gain_err(0.01), alpha_err(0.01), beta_err(0.01),
+     gain_err(0.01), alpha_err(0.01), beta_err(0.04),
      GainSyst(_GainSyst), AlphaSyst(_AlphaSyst), BetaSyst(_BetaSyst)
   {
   }
 
   void CalorimetrySyst::Shift(double sigma, caf::SRSliceProxy *sr, double& weight) const
   {
+
+    // TODO sigma is ambiguous for this kind of multi-param cases; not using it for now
 
     double gain_new = (GainSyst==ana::CaloSyst::kNominal) ? gain : (1. + double(GainSyst)*gain_err)*gain;
     double alpha_new = (AlphaSyst==ana::CaloSyst::kNominal) ? alpha : (1. + double(AlphaSyst)*alpha_err)*alpha; // dummy
@@ -25,12 +30,19 @@ namespace ana {
         for(auto& pt: pfp.trk.calo[i_plane].points){
           auto& this_dedx = pt.dedx;
           if(isnan(this_dedx)) continue;
-          double new_dedx = std::exp( (beta_new*gain_new)/(beta*gain) * std::log(alpha + beta*this_dedx) - alpha_new  ) / beta_new;
+          if(isinf(this_dedx)) continue;
+          double new_dedx = ( std::exp( (beta_new*gain_new)/(beta*gain) * std::log(alpha + (beta/rho/Efield)*this_dedx) ) - alpha_new ) / (beta_new/rho/Efield);
           pt.dedx = new_dedx;
         }
       }
     }
 
   }
+
+  const CalorimetrySyst CalorimetrySyst_BetaUp(ana::CaloSyst::kNominal, ana::CaloSyst::kNominal, ana::CaloSyst::kUp, "CalorimetrySyst_BetaUp", "#beta +4%");
+  const CalorimetrySyst CalorimetrySyst_BetaDown(ana::CaloSyst::kNominal, ana::CaloSyst::kNominal, ana::CaloSyst::kDown, "CalorimetrySyst_BetaDown", "#beta -4%");
+
+  const CalorimetrySyst CalorimetrySyst_GainUp(ana::CaloSyst::kUp, ana::CaloSyst::kNominal, ana::CaloSyst::kNominal, "CalorimetrySyst_GainUp", "gain +1%");
+  const CalorimetrySyst CalorimetrySyst_GainDown(ana::CaloSyst::kDown, ana::CaloSyst::kNominal, ana::CaloSyst::kNominal, "CalorimetrySyst_GainUp", "gain +1%");
 
 } // end namespace ana
--- sbnana/CAFAna/Systs/CalorimetrySysts.h
+++ sbnana/CAFAna/Systs/CalorimetrySysts.h
@@ -23,16 +23,31 @@ namespace ana
 
     void Shift(double sigma, caf::SRSliceProxy *sr, double& weight) const override;
 
-    // Update Nominal parameters
-    inline void UpdateGain(double g, double gerr){ gain = g; gain_err = gerr; };
-    inline void UpdateAlpha(double a, double aerr){ alpha = a; alpha_err = aerr; };
-    inline void UpdateBeta(double b, double berr){ beta = b; beta_err = berr; };
+    // Update detector parameters
+    inline void UpdateTemperature(double t){ temperature = t; }
+    inline void UpdateDensity(double r){ rho = r; }
+    inline void UpdateEfield(double e){ Efield = e; }
+
+    // Update nominal parameters
+    inline void UpdateGain(double g, double gerr){ gain = g; gain_err = gerr; }
+    inline void UpdateAlpha(double a, double aerr){ alpha = a; alpha_err = aerr; }
+    inline void UpdateBeta(double b, double berr){ beta = b; beta_err = berr; }
 
   private:
+
+    // detector parameters
+    double temperature, rho, Efield;
+
     // nominal parameters
     double gain, alpha, beta;
     double gain_err, alpha_err, beta_err; // fractional uncertainty
     CaloSyst GainSyst, AlphaSyst, BetaSyst;
   };
+
+  extern const CalorimetrySyst CalorimetrySyst_BetaUp;
+  extern const CalorimetrySyst CalorimetrySyst_BetaDown;
+  extern const CalorimetrySyst CalorimetrySyst_GainUp;
+  extern const CalorimetrySyst CalorimetrySyst_GainDown;
+
 
 }
