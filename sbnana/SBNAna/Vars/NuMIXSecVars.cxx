e87c901aad3f82809d7341e3071a65dfc8626f59
Jaesung Kim <jae.sung.kim.3426@gmail.com>
2023-10-10 19:50:15 -0500

    Data trigger time is shifted for now..

--- sbnana/SBNAna/Vars/NuMIXSecVars.cxx
+++ sbnana/SBNAna/Vars/NuMIXSecVars.cxx
@@ -81,6 +81,10 @@ namespace ana {
     double triggerTime = 0.;
 
     double foundTriggerTime = sr->hdr.triggerinfo.trigger_within_gate;
+
+    // TODO, Tepmorary? 10/10/2023, JK: Gray's /pnfs/icarus/scratch/users/gputnam/DMCP2023G/reprod/D-* has trigger time is shifted by 4 us
+    if(!sr->hdr.ismc) foundTriggerTime -= 4.; 
+
     if ( !std::isnan(foundTriggerTime) && !std::isinf(foundTriggerTime) && foundTriggerTime < -15. ) triggerTime = -15.;
     else if ( std::isnan(foundTriggerTime) || std::isinf(foundTriggerTime) ) triggerTime = -16.;
     else if ( foundTriggerTime > 30. ) triggerTime = 30.;
