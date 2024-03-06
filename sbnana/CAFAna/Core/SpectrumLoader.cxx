337e8f5e9151ad5ab3be2aa067788099a4b5f3ab
Jacob Larkin <jacob.larkin@stonybrook.edu>
2024-03-06 17:34:42 -0600

    Add offbeam gate count to fNReadouts

--- sbnana/CAFAna/Core/SpectrumLoader.cxx
+++ sbnana/CAFAna/Core/SpectrumLoader.cxx
@@ -192,6 +192,8 @@ namespace ana
     if(!sr->hdr.ismc){
       const int nbnb  = sr->hdr.bnbinfo.size();
       const int nnumi = sr->hdr.numiinfo.size();
+      double noffbeambnb = sr->hdr.noffbeambnb;
+      double noffbeamnumi = sr->hdr.noffbeamnumi;
       if(nbnb > 0 && nnumi > 0){
         std::cout << "SpectrumLoader: nonzero number of both BNB (" << nbnb
                   << ") and NuMI (" << nnumi << ") triggers. I'm confused"
@@ -199,7 +201,7 @@ namespace ana
         abort();
       }
 
-      fNReadouts += nbnb + nnumi;
+      fNReadouts += nbnb + nnumi + noffbeambnb + noffbeamnumi;
     }
 
     // This record was only kept as a receptacle for exposure information. It
