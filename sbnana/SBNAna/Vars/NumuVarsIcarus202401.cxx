4a5c60e089924da0a74dcbf276b6f5fdd4118d5c
Jacob Larkin <jacob.larkin@stonybrook.edu>
2024-05-23 18:57:43 -0500

    Add custom chi2pid calculation

--- sbnana/SBNAna/Vars/NumuVarsIcarus202401.cxx
+++ sbnana/SBNAna/Vars/NumuVarsIcarus202401.cxx
@@ -5,8 +5,142 @@
 #include "sbnanaobj/StandardRecord/Proxy/FwdDeclare.h"
 #include "sbnanaobj/StandardRecord/Proxy/SRProxy.h"
 
+#include "TFile.h"
+#include "TGraph2D.h"
+#include "TMath.h"
+#include "TProfile.h"
+
+#include <string>
+
 namespace ana {
 
+namespace chi2pid {
+
+// Adapted from Jaesung's Chi2 Syst
+struct Chi2PID {
+  Chi2PID() : file_dEdXUncTemplate("template_dEdXUncertainty.root"), file_Chi2Template("dEdxrestemplates.root") {
+    dedx_unc_template = (TGraph2D*)file_dEdXUncTemplate.Get("dEdXRelUncertainty_dEdX_vs_phi");
+    dedx_range_pro = (TProfile*)file_Chi2Template.Get("dedx_range_pro");
+    dedx_range_ka  = (TProfile*)file_Chi2Template.Get("dedx_range_ka");
+    dedx_range_pi  = (TProfile*)file_Chi2Template.Get("dedx_range_pi");
+    dedx_range_mu  = (TProfile*)file_Chi2Template.Get("dedx_range_mu");
+  }
+
+  struct Chi2PIDResult {
+    double chi2_proton, chi2_kaon, chi2_pion, chi2_muon, PIDA;
+    int ndof;
+  };
+
+  Chi2PIDResult calculate_chi2(const caf::Proxy<caf::SRTrackCalo> &calo) const {
+    int npt = 0;
+    double chi2pro = 0;
+    double chi2ka = 0;
+    double chi2pi = 0;
+    double chi2mu = 0;
+    double PIDA = 0; //by Bruce Baller
+    std::vector<double> vpida;
+
+    int used_trkres = 0;
+    for(unsigned i = 0; i < calo.points.size(); ++i) { //hits
+      const auto& pt = calo.points[i];
+      double hit_dedx = pt.dedx;
+      double hit_rr = pt.rr;
+
+      //ignore the first and the last point
+      if (i == 0 || i == calo.points.size() - 1) continue;
+      if (hit_rr < 30) {
+        PIDA += hit_dedx * pow(hit_rr, 0.42);
+        vpida.push_back(hit_dedx * pow(hit_rr, 0.42));
+        used_trkres++;
+      }
+      if (hit_dedx > 100 || hit_dedx < 0.5) continue; //protect against large pulse height
+      int bin = dedx_range_pro->FindBin(hit_rr);
+      if (bin >= 1 && bin <= dedx_range_pro->GetNbinsX()) {
+        double bincpro = dedx_range_pro->GetBinContent(bin);
+        if (bincpro < 1e-6) { //for 0 bin content, using neighboring bins
+          bincpro =
+            (dedx_range_pro->GetBinContent(bin - 1) + dedx_range_pro->GetBinContent(bin + 1)) / 2;
+        }
+        double bincka = dedx_range_ka->GetBinContent(bin);
+        if (bincka < 1e-6) {
+          bincka =
+            (dedx_range_ka->GetBinContent(bin - 1) + dedx_range_ka->GetBinContent(bin + 1)) / 2;
+        }
+        double bincpi = dedx_range_pi->GetBinContent(bin);
+        if (bincpi < 1e-6) {
+          bincpi =
+            (dedx_range_pi->GetBinContent(bin - 1) + dedx_range_pi->GetBinContent(bin + 1)) / 2;
+        }
+        double bincmu = dedx_range_mu->GetBinContent(bin);
+        if (bincmu < 1e-6) {
+          bincmu =
+            (dedx_range_mu->GetBinContent(bin - 1) + dedx_range_mu->GetBinContent(bin + 1)) / 2;
+        }
+        double binepro = dedx_range_pro->GetBinError(bin);
+        if (binepro < 1e-6) {
+          binepro =
+            (dedx_range_pro->GetBinError(bin - 1) + dedx_range_pro->GetBinError(bin + 1)) / 2;
+        }
+        double bineka = dedx_range_ka->GetBinError(bin);
+        if (bineka < 1e-6) {
+          bineka = (dedx_range_ka->GetBinError(bin - 1) + dedx_range_ka->GetBinError(bin + 1)) / 2;
+        }
+        double binepi = dedx_range_pi->GetBinError(bin);
+        if (binepi < 1e-6) {
+          binepi = (dedx_range_pi->GetBinError(bin - 1) + dedx_range_pi->GetBinError(bin + 1)) / 2;
+        }
+        double binemu = dedx_range_mu->GetBinError(bin);
+        if (binemu < 1e-6) {
+          binemu = (dedx_range_mu->GetBinError(bin - 1) + dedx_range_mu->GetBinError(bin + 1)) / 2;
+        }
+        //double errke = 0.05*hit_dedx;   //5% KE resolution
+        double errdedx = 0.04231 + 0.0001783 * hit_dedx * hit_dedx; //resolution on dE/dx
+        errdedx *= hit_dedx;
+        chi2pro += pow((hit_dedx - bincpro) / std::sqrt(pow(binepro, 2) + pow(errdedx, 2)), 2);
+        chi2ka += pow((hit_dedx - bincka) / std::sqrt(pow(bineka, 2) + pow(errdedx, 2)), 2);
+        chi2pi += pow((hit_dedx - bincpi) / std::sqrt(pow(binepi, 2) + pow(errdedx, 2)), 2);
+        chi2mu += pow((hit_dedx - bincmu) / std::sqrt(pow(binemu, 2) + pow(errdedx, 2)), 2);
+        //std::cout<<i<<" "<<hit_dedx<<" "<<hit_rr<<" "<<bincpro<<std::endl;
+        ++npt;
+      }
+    }
+
+    if (npt) {
+      chi2pro /= npt;
+      chi2ka /= npt;
+      chi2pi /= npt;
+      chi2mu /= npt;
+    }
+    static bool fUseMedian = true; // I think default fcl in ICARUS is true
+    if (used_trkres > 0) {
+      if (fUseMedian) {
+        PIDA = TMath::Median(vpida.size(), &vpida[0]);
+      }
+      else { // use mean
+        PIDA /= used_trkres;
+      }
+    }
+
+    // Making output
+    Chi2PIDResult output;
+    output.chi2_kaon = chi2ka;
+    output.chi2_muon = chi2mu;
+    output.chi2_pion = chi2pi;
+    output.chi2_proton = chi2pro;
+    output.PIDA = PIDA;
+    output.ndof = npt;
+
+    return output;
+  }
+
+  TFile file_dEdXUncTemplate, file_Chi2Template;
+  TGraph2D *dedx_unc_template;
+  TProfile *dedx_range_pro, *dedx_range_ka, *dedx_range_pi, *dedx_range_mu;
+};
+
+static Chi2PID chi2_calculator;
+
+}
 
 // Mostly just copied from NumuVarsIcarus202106.cxx
 const Var kIcarus202401MuonIdx([](const caf::SRSliceProxy* slc) -> int {
@@ -25,8 +159,11 @@ const Var kIcarus202401MuonIdx([](const caf::SRSliceProxy* slc) -> int {
         //int plane = trk.calo[1].nhit > trk.calo[2].nhit ? 1 : 2;
         int plane = 2; // Hard code collection plane for now since induction 2 has peak at higher chi2
 
-        float Chi2Proton = trk.chi2pid[plane].chi2_proton;
-        float Chi2Muon = trk.chi2pid[plane].chi2_muon;
+        //float Chi2Proton = trk.chi2pid[plane].chi2_proton;
+        //float Chi2Muon = trk.chi2pid[plane].chi2_muon;
+        auto chi2 = chi2pid::chi2_calculator.calculate_chi2(trk.calo[plane]);
+        float Chi2Proton = chi2.chi2_proton;
+        float Chi2Muon = chi2.chi2_muon;
   
         const bool Contained = ( !isnan(trk.end.x) &&
             (( trk.end.x < -61.94 - 5 && trk.end.x > -358.49 + 5 ) ||
@@ -49,7 +186,8 @@ static bool Icarus202401_proton_cut(const caf::SRTrackProxy& trk)
 {
   //int plane = trk.calo[1].nhit > trk.calo[2].nhit ? 1 : 2;
   int plane = 2; // Hard code collection plane for now since induction 2 has peak at higher chi2
-  return trk.chi2pid[plane].chi2_proton < 100 && trk.chi2pid[plane].chi2_proton > 0;
+  auto chi2 = chi2pid::chi2_calculator.calculate_chi2(trk.calo[plane]);
+  return chi2.chi2_proton < 100 && chi2.chi2_proton > 0;
 }
 
 const Var kIcarus202401NumPions([](const caf::SRSliceProxy* slc)
@@ -65,7 +203,7 @@ const Var kIcarus202401NumPions([](const caf::SRSliceProxy* slc)
                                    slc->vertex.y - trk.start.y,
                                    slc->vertex.z - trk.start.z);
     const bool AtSlice = ( Atslc < 10.0 && pfp.parent_is_primary);
-    if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
+    //if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
     if(pfp.id != muID && !Icarus202401_proton_cut(trk) && AtSlice && std::hypot(pfp.trk.rangeP.p_pion, 0.140f) - 0.14 > 0.025)
       ++count;
   }
@@ -80,7 +218,7 @@ const Var kIcarus202401NumProtons([](const caf::SRSliceProxy* slc)
   if (idx >= 0) muID = slc->reco.pfp.at(idx).id;
   for(auto& pfp: slc->reco.pfp) {
     if (pfp.trackScore < 0.4) { continue; }
-    if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
+    //if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
     auto const& trk = pfp.trk;
     const float Atslc = std::hypot(slc->vertex.x - trk.start.x,
                                    slc->vertex.y - trk.start.y,
@@ -99,8 +237,7 @@ const Var kIcarus202401NumShowers([](const caf::SRSliceProxy* slc){
     const float Atslc = std::hypot(slc->vertex.x - pfp.shw.start.x,
                                    slc->vertex.y - pfp.shw.start.y,
                                    slc->vertex.z - pfp.shw.start.z);
-    //const bool AtSlice = ( Atslc < 10.0 && pfp.parent_is_primary);
-    const bool AtSlice = pfp.parent_is_primary;
+    const bool AtSlice = ( Atslc < 50.0 && pfp.parent_is_primary);
     const bool isShower = pfp.trackScore > 0 && (pfp.trackScore < 0.4 || (pfp.trackScore < 0.5 && !Icarus202401_proton_cut(pfp.trk)));
     if(isShower && AtSlice && pfp.shw.plane[2].energy > 0.025) count++;
   }
@@ -153,7 +290,6 @@ const MultiVar kIcarus202401RecoProtonP([](const caf::SRSliceProxy* slc){
   for(const auto& pfp: slc->reco.pfp) {
     if(pfp.id == muID) continue;
     if(pfp.trackScore < 0.4) continue;
-    if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
     if(Icarus202401_proton_cut(pfp.trk) && pfp.parent_is_primary)
       Ps.push_back(pfp.trk.rangeP.p_proton);
   }
@@ -168,7 +304,6 @@ const MultiVar kIcarus202401RecoPionP([](const caf::SRSliceProxy* slc){
   for(const auto& pfp: slc->reco.pfp) {
     if(pfp.id == muID) continue;
     if(pfp.trackScore < 0.5) continue;
-    if(pfp.trk.chi2pid[2].chi2_proton == 0 && pfp.trk.chi2pid[1].chi2_proton == 0) continue;
     if(!Icarus202401_proton_cut(pfp.trk) && pfp.parent_is_primary)
       Ps.push_back(pfp.trk.rangeP.p_proton);
   }
