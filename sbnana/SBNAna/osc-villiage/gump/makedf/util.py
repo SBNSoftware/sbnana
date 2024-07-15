b816db0e98821a7139e47969a044fb4d55935798
Mun Jung Jung <wjdanswjddl@gmail.com>
2024-07-15 22:00:21 +0000

    sbnd fv

--- sbnana/SBNAna/osc-villiage/gump/makedf/util.py
+++ sbnana/SBNAna/osc-villiage/gump/makedf/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+import sys
 
 def mag(x, y, z):
     return np.sqrt(x**2 + y**2 + z**2)
@@ -15,32 +16,47 @@ def dotdf(df1, df2):
 def unitdf(df):
     return df.divide(magdf(df), axis=0)
 
-def InFV(df, inzback, inx=10, iny=10, inzfront=10):
-    xmin_C0 = -358.49
-    xmax_C0 = -61.94
+def InFV(df, inzback, inx=10, iny=10, inzfront=10, det="ICARUS"):
+    if det == "ICARUS":
+        xmin_C0 = -358.49
+        xmax_C0 = -61.94
 
-    xmax_C1 = -xmin_C0
-    xmin_C1 = -xmax_C0
+        xmax_C1 = -xmin_C0
+        xmin_C1 = -xmax_C0
 
-    ymin = -181.85999999999999
-    ymax = 134.96
+        ymin = -181.85999999999999
+        ymax = 134.96
 
-    zmin = -894.950652270838
-    zmax = 894.950652270838
+        zmin = -894.950652270838
+        zmax = 894.950652270838
 
-    xmin_C0 = xmin_C0 + inx
-    xmax_C0 = xmax_C0 - inx
-    xmin_C1 = xmin_C1 + inx
-    xmax_C1 = xmax_C1 - inx
+        xmin_C0 = xmin_C0 + inx
+        xmax_C0 = xmax_C0 - inx
+        xmin_C1 = xmin_C1 + inx
+        xmax_C1 = xmax_C1 - inx
+
+        ymin = ymin + iny
+        ymax = ymax - iny
+
+        zmin = zmin + inzfront
+        zmax = zmax - inzback
+
+        return (((df.x < xmax_C0) & (df.x > xmin_C0)) | ((df.x < xmax_C1) & (df.x > xmin_C1))) &\
+            (df.y < ymax) & (df.y > ymin) & (df.z < zmax) & (df.z > zmin)
     
-    ymin = ymin + iny
-    ymax = ymax - iny
+    elif det == "SBND":
+        xmin = -199.15 + 10
+        ymin = -200. + 10
+        zmin = 0.0 + 10
+        xmax = 199.15 - 10
+        ymax =  200. - 10
+        zmax =  500. - 50
+        return (df.x > xmin) & (df.x < xmax) & (df.y > ymin) & (df.y < ymax) & (df.z > zmin) & (df.z < zmax)
+    
+    else:
+        raise NameError("DETECTOR not valid, should be SBND or ICARUS")
 
-    zmin = zmin + inzfront
-    zmax = zmax - inzback
-
-    return (((df.x < xmax_C0) & (df.x > xmin_C0)) | ((df.x < xmax_C1) & (df.x > xmin_C1))) &\
-        (df.y < ymax) & (df.y > ymin) & (df.z < zmax) & (df.z > zmin)
+        
 
 def TrkInFV(df):
     return InFV(df, 15.)
