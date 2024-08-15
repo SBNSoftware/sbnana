e68b405a4c5fe361b20088678b81b28f8cb7bf6a
Jamie Dyer <jamie.dyer@colostate.edu>
2024-08-15 13:44:52 -0500

    Fix more file path dependencies. Fix branch selection in preselection (Note that though the logic was wrong previously, this ~never led to the incorrect branch being chosen. See SBN-docDB-37044 for more details).

--- sbnana/SBNAna/icarus-analysis-villiage/pyana/pyanalib/ntuple_glob.py
+++ sbnana/SBNAna/icarus-analysis-villiage/pyana/pyanalib/ntuple_glob.py
@@ -42,11 +42,11 @@ def _loaddf(applyfs, g):
     # fname, index, applyfs = inp
     index, fname = g
     # Convert pnfs to xroot URL's
-    if fname.startswith("/pnfs"):
-        fname = fname.replace("/pnfs", "root://fndcadoor.fnal.gov:1094/pnfs/fnal.gov/usr")
+    #if fname.startswith("/pnfs"):
+    #    fname = fname.replace("/pnfs", "root://fndcadoor.fnal.gov:1094/pnfs/fnal.gov/usr")
     # fix xroot URL's
-    elif fname.startswith("xroot"):
-        fname = fname[1:]
+    #elif fname.startswith("xroot"):
+    #    fname = fname[1:]
 
     madef = False
 
