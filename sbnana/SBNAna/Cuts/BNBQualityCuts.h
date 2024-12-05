195a2f76f2e8c8895aaa5a443a426a10f7dc5bf7
Jacob Smith <jacob.a.smith@stonybrook.edu>
2024-12-05 14:20:43 -0600

    Initial commit of SpillCuts that can be used to filter the BNB for quality data. These SpillCuts act on BNB quality-monitoring devices such as TOR860 and THCURR. See documentation in files for more information.

---
+++ sbnana/SBNAna/Cuts/BNBQualityCuts.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "sbnana/CAFAna/Core/Cut.h"
+
+namespace ana {
+
+extern const SpillCut kTOR860;
+extern const SpillCut kTOR875;
+
+extern const SpillCut kLM875A;
+extern const SpillCut kLM875B;
+extern const SpillCut kLM875C;
+
+extern const SpillCut kHP875;
+extern const SpillCut kVP875;
+
+extern const SpillCut kHPTG1; 
+extern const SpillCut kVPTG1;
+
+extern const SpillCut kHPTG2;
+extern const SpillCut kVPTG2;
+
+/*
+   This cut is left commented out since it is hard to implement but may be of
+   future use. See the BNBQualityCuts_<Nov2024, Jan2025>.cxx files for more
+   information. -- Jacob Smith
+
+extern const SpillCut kBTJT2; 
+*/
+
+extern const SpillCut kTHCURR;
+
+// ^--- INDIVIDUAL CUTS ; MASTER CUT ---v
+
+extern const SpillCut kBNBQuality_noBPMs_10Feb2025;
+extern const SpillCut kBNBQuality; // combination of individual cuts given above
+                                   // NB: NOT IN USE AS OF Feb. 10th, 2025
+}
