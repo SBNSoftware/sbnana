43de760bd4fcae14b3225961bf8ce70dbad717e4
Jacob Smith <jacob.a.smith@stonybrook.edu>
2025-02-12 16:21:03 -0600

    Update code structure to address spill-matching bug resulting in cutting of ~95% of spills (bad). New structure uses SIMPLESPILLVAR() function to create SpillVars and make SpillCuts on these SpillVars like what is done with regular Vars and Cuts.

--- sbnana/SBNAna/Cuts/BNBQualityCuts.cxx
+++ sbnana/SBNAna/Cuts/BNBQualityCuts.cxx
@@ -1,366 +1,85 @@
-#include "sbnana/CAFAna/Core/Cut.h"
-#include "sbnana/SBNAna/Cuts/BNBQualityCuts.h"
+///////////////////////////////////////////////////////////////////////////////
+// File: BNBQualityCuts.cxx                                                  //
+// Author: Jacob Smith (smithja)                                             //
+// Last edited: February 12th, 2025                                          //
+//                                                                           //
+// Explicit definitions of the BNB Quality Cuts used for the ICARUS          //
+// experiment.                                                               //
+///////////////////////////////////////////////////////////////////////////////
+
+#include "sbnana/SBNAna/Vars/BNBVars.h"
+
 #include "sbnanaobj/StandardRecord/Proxy/SRProxy.h"
-#include <iostream>
 
 namespace ana { // use namespace ana for entire file
+/*
+    While there is sometimes discernable structure in a given BNB quality 
+    variable (e.g. TOR860 and TOR875 stay around 4.5E12 POT when only the BNB is
+    on and 3.0E12 POT when both the BNB and NuMI beams are on), exact behavior 
+    of such variables is not known in detail as a function of time. Thus, the 
+    latest (read 'final') version of a given cut will be the most stringent cut
+     we can make with a limited knowledge of the detailed behavior of that cut.
+*/
+static constexpr double TOR860_LB = +100e9; // units: POT
+static constexpr double TOR875_LB = +100e9; 
+
+static constexpr double LM875A_LB = +1e-2; // units: rad/s
+static constexpr double LM875B_LB = +1e-2;
+static constexpr double LM875C_LB = +1e-2;
+
+/* 
+    SMITHJA: I am waiting on the External Beams group to provide a table
+    of the nominal beam position monitor (BPM) variables for all of the ICARUS
+    data runs. Until that time, the cuts for the BPM variables are my personal
+    best guess after looking at these variables for ICARUS's Run 2 data. 
+*/
+static constexpr double HP875_LB = -0.25, HP875_UB = +0.25; // units: mm
+static constexpr double VP875_LB = -0.25, VP875_UB = +0.30;
+static constexpr double HPTG1_LB = -0.25, HPTG1_UB = +0.25;
+static constexpr double VPTG1_LB = -0.20, VPTG1_UB = +0.30;
+static constexpr double HPTG2_LB = -0.25, HPTG2_UB = +0.25;
+static constexpr double VPTG2_LB = -0.20, VPTG2_UB = +0.30;
 
 /*
-   Here are the hard-coded physically meaningful bounds for BNB quality
-   SpillCuts. 
+    The BTJT2 variable is somewhat unreliable to use as a spill-by-spill cut.
+    BTJT2 measures the temperature of the BNB near the target. Since runs can be
+    started/stopped more quickly than the thermal fluctuations of BTJT2, there
+    can be some correlation between spills. For this reason BTJT2 is deemed to 
+    have diminishing returns compared to other BNB quality variables listed. 
+    You may employ cuts on BTJT2 at your own peril.
 */
+// static constexpr double BTJT2_LB = 999, BTJT2_UB = 999; // units: deg C
 
-/*
-   While there is sometimes discernable structure in a given BNB quality 
-   variable (e.g. TOR860 and TOR875 stay around 4.5E12 POT when only the BNB is
-   on and 3.0E12 POT when both the BNB and NuMI beams are on), exact behavior of
-   such variables is not known in detail as a function of time. Thus, the latest 
-   (read 'final') version of a given cut will be the most stringent cut we can 
-   make with a limited knowledge of the detailed behavior of that cut.
+
+/* 
+    SMITHJA: Further cuts on THCURR could/should be made since it was discovered 
+    that the horn current is not always centered around 174 kA. 
+    Further work is needed to understand horn current for different runs. The
+    cut ranging from 173 to 175 kA is for ICARUS Run 2 data. Generally it was
+    found that a range of 173 to 177 included the horn currents for a majority
+    of ICARUS data runs.
 */
+// static constexpr double THCURR_LB = +173, THCURR_UB = +177; // units: kA
+static constexpr double THCURR_LB = +173, THCURR_UB = +175; // units: kA
 
-const double TOR860_LB = +100e9; // units: POT
-const double TOR875_LB = +100e9; 
-
-const double LM875A_LB = +1e-2; // units: rad/s
-const double LM875B_LB = +1e-2;
-const double LM875C_LB = +1e-2;
-
-// SMITHJA: I will have to get in touch with (Tom from) the External Beams group
-//          at Fermilab to get final cuts on these beam position monitor (BPM)
-//          variables since they change from run to run. If realtime values for 
-//          those optimal BPM variables are not accessible (which is the likely
-//          case), the BPM cuts will likely be hard-coded here. 
-const double HP875_LB = -0.25;  const double HP875_UB = +0.25; // units: mm
-const double VP875_LB = -0.20;  const double VP875_UB = +0.30;
-const double HPTG1_LB = -0.25;  const double HPTG1_UB = +0.25;
-const double VPTG1_LB = -0.20;  const double VPTG1_UB = +0.30;
-const double HPTG2_LB = -0.25;  const double HPTG2_UB = +0.25;
-const double VPTG2_LB = -0.20;  const double VPTG2_UB = +0.30;
-
-/*
-   The BTJT2 variable is somewhat unreliable to use as a spill-by-spill cut.
-   BTJT2 measures the temperature of the BNB near the target. Since runs can be
-   started/stopped more quickly than the thermal fluctuations of BTJT2, there
-   can be some amount of "cross contamination" between spills. For this reason
-   BTJT2 is deemed to have diminishing returns compared to other BNB quality
-   variables listed. You may employ cuts on BTJT2 at your own peril.
-
-const double BTJT2_LB = 999;  const double BTJT2_UB = 999; // units: deg C
-*/
-
-const double THCURR_LB = +173;  const double THCURR_UB = +177; // units: kA
-
-/* SMITHJA:
-   Further cuts on THCURR could/should be made since it was discovered that
-   the horn current (i.e. THCURR) is not always centered around 174 kA. Further
-   work needed to understand horn current for different runs. -- Jacob Smith
-
-const double THCURR_LB = +173;  const double THCURR_UB = +175; // units: kA
-*/
 //-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
+const SpillCut kTOR860Cut = kSpillTOR860 >= TOR860_LB;
+const SpillCut kTOR875Cut = kSpillTOR875 >= TOR875_LB;
 
+const SpillCut kLM875ACut = kSpillLM875A >= LM875A_LB;
+const SpillCut kLM875BCut = kSpillLM875B >= LM875B_LB;
+const SpillCut kLM875CCut = kSpillLM875C >= LM875C_LB;
 
-struct SpillInfo { // contains information about a spill of the BNB
-  // These are the variables that measure the quality of a spill in the BNB.
-  float TOR860, TOR875, \
-        LM875A, LM875B, LM875C, \
-        HP875, VP875, \
-        HPTG1, VPTG1, \
-        HPTG2, VPTG2, \
-//        BTJT2,
-        THCURR;
-  
-  // These are used to identify/match spills in the global vector of 
-  // SpillInfo objects (see GLOBAL_SPILL_INFO below).
-  unsigned int run, event;
-};
+const SpillCut kHP875Cut = kSpillHP875 >= HP875_LB && kSpillHP875 <= HP875_UB;
+const SpillCut kVP875Cut = kSpillVP875 >= VP875_LB && kSpillVP875 <= VP875_UB;
+const SpillCut kHPTG1Cut = kSpillHPTG1 >= HPTG1_LB && kSpillHPTG1 <= HPTG1_UB;
+const SpillCut kVPTG1Cut = kSpillVPTG1 >= VPTG1_LB && kSpillVPTG1 <= VPTG1_UB;
+const SpillCut kHPTG2Cut = kSpillHPTG2 >= HPTG2_LB && kSpillHPTG2 <= HPTG2_UB;
+const SpillCut kVPTG2Cut = kSpillVPTG2 >= VPTG2_LB && kSpillVPTG2 <= VPTG2_UB;
 
-// vector containing all of the information for spills of the BNB:
-static std::vector<SpillInfo> GLOBAL_SPILL_INFO; 
+// DEPRECIATED: see above notes about BTJT2
+//const SpillCut kBTJT2Cut = kSpillBTJT2 >= BTJT2_LB && kSpillBTJT2 <= BTJT2_UB;
 
-
-/*
-   This function sets all of the values for a SpillInfo object (see above).
-
-Params:
-  * run: integer identifier for the run/event
-  * info: this contains all of the BNB monitoring info for a given spill 
-          (including the event number, which is also used to identify a
-           run/event)
-*/
-void fill_spill_info( unsigned int run, const caf::Proxy<caf::SRBNBInfo> &info){
-  GLOBAL_SPILL_INFO.emplace_back();
-
-  GLOBAL_SPILL_INFO.back().run = run;
-  GLOBAL_SPILL_INFO.back().event = info.event;
-
-  GLOBAL_SPILL_INFO.back().TOR860 = info.TOR860;
-  GLOBAL_SPILL_INFO.back().TOR875 = info.TOR875;
-  GLOBAL_SPILL_INFO.back().LM875A = info.LM875A;
-  GLOBAL_SPILL_INFO.back().LM875B = info.LM875B;
-  GLOBAL_SPILL_INFO.back().LM875C = info.LM875C;
-  GLOBAL_SPILL_INFO.back().HP875  = info.HP875;
-  GLOBAL_SPILL_INFO.back().VP875  = info.VP875;
-  GLOBAL_SPILL_INFO.back().HPTG1  = info.HPTG1;
-  GLOBAL_SPILL_INFO.back().VPTG1  = info.VPTG1;
-  GLOBAL_SPILL_INFO.back().HPTG2  = info.HPTG2;
-  GLOBAL_SPILL_INFO.back().VPTG2  = info.VPTG2;
-//  GLOBAL_SPILL_INFO.back().BTJT2  = info.BTJT2;
-  GLOBAL_SPILL_INFO.back().THCURR = info.THCURR;
-}
-
-const SpillCut kTOR860([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){                           // non-zero nbnb indicates new subrun...
-        GLOBAL_SPILL_INFO.clear();                   // ...so we get rid of spills tied to last subrun...
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ // ...and fill in info for this subrun's spills
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; // ensure event matching
-        // make preliminary cut on variable of interest --v
-        if( match.TOR860 >= TOR860_LB) return true; 
-    }
-    return false; // a spill does not pass a cut if 
-                  // there is no event-matched data
-});
-
-/*
-   NOTE: For brevity of commenting, the preliminary SpillCuts below are not 
-         commented; however, they are the same as kTOR860 (see above) but with
-         different variables of interest, e.g. TOR875, LM875B, THCURR, etc.
-*/
-
-const SpillCut kTOR875([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){                               
-        GLOBAL_SPILL_INFO.clear();                   
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.TOR875 >= TOR875_LB) return true; 
-    }
-    return false; 
-});
-
-const SpillCut kLM875A([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){                               
-        GLOBAL_SPILL_INFO.clear();                   
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.LM875A >= LM875A_LB) return true; 
-    }
-    return false; 
-});
-
-const SpillCut kLM875B([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){                               
-        GLOBAL_SPILL_INFO.clear();                   
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.LM875B >= LM875B_LB) return true; 
-    }
-    return false; 
-});
-
-const SpillCut kLM875C([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){                               
-        GLOBAL_SPILL_INFO.clear();                   
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.LM875C >= LM875C_LB) return true; 
-    }
-    return false; 
-});
-
-const SpillCut kHP875([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.HP875 >= HP875_LB && match.HP875 <= HP875_UB) return true;
-    }
-    return false; 
-});
-
-const SpillCut kVP875([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.VP875 >= VP875_LB && match.VP875 <= VP875_UB) return true;
-    }
-    return false; 
-});
-
-const SpillCut kHPTG1([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.HPTG1 >= HPTG1_LB && match.HPTG1 <= HPTG1_UB) return true;
-    }
-    return false; 
-});
-
-const SpillCut kVPTG1([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.VPTG1 >= VPTG1_LB && match.VPTG1 <= VPTG1_UB) return true;
-    }
-    return false; 
-});
-
-const SpillCut kHPTG2([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.HPTG2 >= HPTG2_LB && match.HPTG2 <= HPTG2_UB) return true;
-    }
-    return false; 
-});
-
-const SpillCut kVPTG2([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.VPTG2 >= VPTG2_LB && match.VPTG2 <= VPTG2_UB) return true;
-    }
-    return false; 
-});
-/*
-const SpillCut kBTJT2([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.BTJT2 >= BTJT2_LB && match.BTJT2 < BTJT2_UB) return true;
-    }
-    return false; 
-});
-*/
-const SpillCut kTHCURR([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.THCURR >= THCURR_LB && match.THCURR <= THCURR_UB) return true;
-    }
-    return false; 
-});
-
-/* -------------------------------------- \
-//                                        |
-// ^--- INDIVIDUAL CUTS ; MASTER CUT ---v |
-//                                        |
-// ------------------------------------- */
-
-const SpillCut kBNBQuality_noBPMs_10Feb2025([](const caf::SRSpillProxy* sr) {
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){
-            fill_spill_info( sr->hdr.run, bnbinfo);
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue;
-        if( match.TOR860 >= TOR860_LB && \
-              match.TOR875 >= TOR875_LB && \
-              match.LM875A >= LM875A_LB && \
-              match.LM875B >= LM875B_LB && \
-              match.LM875C >= LM875C_LB && \
-//              match.HP875  >= HP875_LB  && match.HP875 <= HP875_UB  && 
-//              match.VP875  >= VP875_LB  && match.VP875 <= VP875_UB  && 
-//              match.HPTG1  >= HPTG1_LB  && match.HPTG1 <= HPTG1_UB  && 
-//              match.VPTG1  >= VPTG1_LB  && match.VPTG1 <= VPTG1_UB  && 
-//              match.HPTG2  >= HPTG2_LB  && match.HPTG2 <= HPTG2_UB  && 
-//              match.VPTG2  >= VPTG2_LB  && match.VPTG2 <= VPTG2_UB  && 
-//              match.BTJT2  >= BTJT2_LB  && match.BTJT2  < BTJT2_UB  &&
-              match.THCURR >= THCURR_LB && match.THCURR <= THCURR_UB) {
-            return true;
-        }
-    }
-    return false;
-});
-
-const SpillCut kBNBQuality([](const caf::SRSpillProxy* sr){
-    if( sr->hdr.nbnbinfo){
-        GLOBAL_SPILL_INFO.clear();
-        for( const auto &bnbinfo : sr->hdr.bnbinfo){ 
-            fill_spill_info( sr->hdr.run, bnbinfo);  
-        }
-    }
-    for( const auto& match: GLOBAL_SPILL_INFO) {
-        if( match.event != sr->hdr.evt) continue; 
-        if( match.TOR860 >= TOR860_LB && \
-              match.TOR875 >= TOR875_LB && \
-              match.LM875A >= LM875A_LB && \
-              match.LM875B >= LM875B_LB && \
-              match.LM875C >= LM875C_LB && \
-              match.HP875  >= HP875_LB  && match.HP875 <= HP875_UB  && \
-              match.VP875  >= VP875_LB  && match.VP875 <= VP875_UB  && \
-              match.HPTG1  >= HPTG1_LB  && match.HPTG1 <= HPTG1_UB  && \
-              match.VPTG1  >= VPTG1_LB  && match.VPTG1 <= VPTG1_UB  && \
-              match.HPTG2  >= HPTG2_LB  && match.HPTG2 <= HPTG2_UB  && \
-              match.VPTG2  >= VPTG2_LB  && match.VPTG2 <= VPTG2_UB  && \
-//              match.BTJT2  >= BTJT2_LB  && match.BTJT2  < BTJT2_UB  &&
-              match.THCURR >= THCURR_LB && match.THCURR <= THCURR_UB) {
-            return true;
-        }
-    }
-    return false; 
-});
-
+const SpillCut kTHCURRCut = kSpillTHCURR >= THCURR_LB && kSpillTHCURR <= THCURR_UB;
 } // stop using namespace ana
--- sbnana/SBNAna/Cuts/BNBQualityCuts.h
+++ sbnana/SBNAna/Cuts/BNBQualityCuts.h
@@ -1,38 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////
+// File: BNBQualityCuts.h                                                    //
+// Author: Jacob Smith (smithja)                                             //
+// Last edited: February 12th, 2025                                          //
+//                                                                           //
+// Header file to define all of the BNB Quality Cuts for the ICARUS          //
+// experiment.                                                               //
+///////////////////////////////////////////////////////////////////////////////
+
 #pragma once
 
 #include "sbnana/CAFAna/Core/Cut.h"
 
 namespace ana {
+    extern const SpillCut kTOR860Cut;
+    extern const SpillCut kTOR875Cut;
 
-extern const SpillCut kTOR860;
-extern const SpillCut kTOR875;
+    extern const SpillCut kLM875ACut;
+    extern const SpillCut kLM875BCut;
+    extern const SpillCut kLM875CCut;
 
-extern const SpillCut kLM875A;
-extern const SpillCut kLM875B;
-extern const SpillCut kLM875C;
+    extern const SpillCut kHP875Cut;
+    extern const SpillCut kVP875Cut;
 
-extern const SpillCut kHP875;
-extern const SpillCut kVP875;
+    extern const SpillCut kHPTG1Cut; 
+    extern const SpillCut kVPTG1Cut;
 
-extern const SpillCut kHPTG1; 
-extern const SpillCut kVPTG1;
-
-extern const SpillCut kHPTG2;
-extern const SpillCut kVPTG2;
+    extern const SpillCut kHPTG2Cut;
+    extern const SpillCut kVPTG2Cut;
 
 /*
-   This cut is left commented out since it is hard to implement but may be of
-   future use. See the BNBQualityCuts_<Nov2024, Jan2025>.cxx files for more
-   information. -- Jacob Smith
-
-extern const SpillCut kBTJT2; 
+    This cut is left commented out since it is hard to implement but may be of
+    future use. See the BNBQualityCuts.cxx for more information.
 */
+//     extern const SpillCut kBTJT2;
 
-extern const SpillCut kTHCURR;
+    extern const SpillCut kTHCURRCut;
 
-// ^--- INDIVIDUAL CUTS ; MASTER CUT ---v
+    // ^--- INDIVIDUAL CUTS ; COMBINATION CUTS ---v
 
-extern const SpillCut kBNBQuality_noBPMs_10Feb2025;
-extern const SpillCut kBNBQuality; // combination of individual cuts given above
-                                   // NB: NOT IN USE AS OF Feb. 10th, 2025
+    extern const SpillCut kBNBQuality_noBPMs_10Feb2025 = \
+        kTOR860Cut && kTOR875Cut && \
+        kLM875ACut && kLM875BCut && kLM875CCut && \
+//        kBTJT2Cut &&
+        kTHCURRCut;
+
+    extern const SpillCut kBNBQuality = \ // all individual cuts 
+        kBNBQuality_noBPMs_10Feb2025 && \ // NOT IN USE AS OF Feb. 10th, 2025
+        kHP875Cut && kVP875Cut && \
+        kHPTG1Cut && kVPTG1Cut && \
+        kHPTG2Cut && kVPTG2Cut;
 }
