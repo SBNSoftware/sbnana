901472a408dd7179c8b16f4b29e894d2f1b835a6
Jacob Smith <jacob.a.smith@stonybrook.edu>
2025-02-28 12:38:48 -0600

    Tested MicroBooNE beam position monitor offsets. These allowed zero spills to pass. Resetting to no offset case but keeping this code for historical purposes.

--- sbnana/SBNAna/Cuts/BNBQualityCuts.cxx
+++ sbnana/SBNAna/Cuts/BNBQualityCuts.cxx
@@ -33,9 +33,34 @@ const double LM875C_LB = +1e-2;
 /* 
     SMITHJA: I am waiting on the External Beams group to provide a table
     of the nominal beam position monitor (BPM) variables for all of the ICARUS
-    data runs. Until that time, the cuts for the BPM variables are my personal
-    best guess after looking at these variables for ICARUS's Run 2 data. 
+    data runs. Until that time, the cuts for the BPM variables are the nominal
+    BPM values from MicroBooNE's getFOM2.cxx file created in part by Zarko
+    Pavlovic and located at 
+    https://github.com/uboone/ubraw/blob/develop/ubraw/BeamData/getFOM2.cxx 
+
+    Upper and lower bound windows can be tweaked as needed. But if we continue
+    to use the nominal BPM values from MicroBooNE, these should be encoded in
+    the offsets below. These offsets are taken from getFOM2.cxx linked above.
 */
+double hp875_offset = -3.40271; // units: mm
+double vp875_offset = +1.47511;
+double hptg1_offset = +0.457014;
+double vptg1_offset = +0.38914;
+double hptg2_offset = +0.8;
+double vptg2_offset = +1.0;
+
+/* 
+    SMITHJA: The MicroBooNE offsets allow no spills to pass through. We are
+    instead re-setting the offsets to zero and hoping External Beams will 
+    come through...
+*/
+hp875_offset = 0; // units: mm
+vp875_offset = 0;
+hptg1_offset = 0;
+vptg1_offset = 0;
+hptg2_offset = 0;
+vptg2_offset = 0;
+
 const double HP875_LB = -0.25, HP875_UB = +0.25; // units: mm
 const double VP875_LB = -0.25, VP875_UB = +0.30;
 const double HPTG1_LB = -0.25, HPTG1_UB = +0.25;
@@ -51,6 +76,7 @@ const double VPTG2_LB = -0.20, VPTG2_UB = +0.30;
     have diminishing returns compared to other BNB quality variables listed. 
     You may employ cuts on BTJT2 at your own peril.
 */
+
 // const double BTJT2_LB = 999, BTJT2_UB = 999; // units: deg C
 
 
@@ -74,12 +100,29 @@ const SpillCut kLM875ACut = kSpillLM875A >= LM875A_LB;
 const SpillCut kLM875BCut = kSpillLM875B >= LM875B_LB;
 const SpillCut kLM875CCut = kSpillLM875C >= LM875C_LB;
 
-const SpillCut kHP875Cut = kSpillHP875 >= HP875_LB && kSpillHP875 <= HP875_UB;
-const SpillCut kVP875Cut = kSpillVP875 >= VP875_LB && kSpillVP875 <= VP875_UB;
-const SpillCut kHPTG1Cut = kSpillHPTG1 >= HPTG1_LB && kSpillHPTG1 <= HPTG1_UB;
-const SpillCut kVPTG1Cut = kSpillVPTG1 >= VPTG1_LB && kSpillVPTG1 <= VPTG1_UB;
-const SpillCut kHPTG2Cut = kSpillHPTG2 >= HPTG2_LB && kSpillHPTG2 <= HPTG2_UB;
-const SpillCut kVPTG2Cut = kSpillVPTG2 >= VPTG2_LB && kSpillVPTG2 <= VPTG2_UB;
+const SpillCut kHP875Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillHP875(sr) + hp875_offset) >= HP875_LB && ( kSpillHP875(sr) + hp875_offset) <= HP875_UB;
+} );
+
+const SpillCut kVP875Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillVP875(sr) + vp875_offset) >= VP875_LB && ( kSpillVP875(sr) + vp875_offset) <= VP875_UB;
+} );
+
+const SpillCut kHPTG1Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillHPTG1(sr) + hptg1_offset) >= HPTG1_LB && ( kSpillHPTG1(sr) + hptg1_offset) <= HPTG1_UB;
+} );
+
+const SpillCut kVPTG1Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillVPTG1(sr) + vptg1_offset) >= VPTG1_LB && ( kSpillVPTG1(sr) + vptg1_offset) <= VPTG1_UB;
+} );
+
+const SpillCut kHPTG2Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillHPTG2(sr) + hptg2_offset) >= HPTG2_LB && ( kSpillHPTG2(sr) + hptg2_offset) <= HPTG2_UB;
+} );
+
+const SpillCut kVPTG2Cut  ( [](const caf::SRSpillProxy* sr) { 
+    return ( kSpillVPTG2(sr) + vptg2_offset) >= VPTG2_LB && ( kSpillVPTG2(sr) + vptg2_offset) <= VPTG2_UB;
+} );
 
 // DEPRECIATED: see above notes about BTJT2
 //const SpillCut kBTJT2Cut = kSpillBTJT2 >= BTJT2_LB && kSpillBTJT2 <= BTJT2_UB;
@@ -89,5 +132,9 @@ const SpillCut kTHCURRCut = kSpillTHCURR >= THCURR_LB && kSpillTHCURR <= THCURR_
 // ^--- INDIVIDUAL CUTS ; COMBINATION CUTS ---v
 
 const SpillCut kBNBQualityCut_noBPMs_10Feb2025 = kTOR860Cut && kTOR875Cut && kLM875ACut && kLM875BCut && kLM875CCut && kTHCURRCut;
+
+// WARNING: Beam position monitor (BPM) cuts are not finalized as of February
+//          20th, 2025. 
+const SpillCut kBNBQualityCut_20Feb2025 = kBNBQualityCut_noBPMs_10Feb2025 && kHP875Cut && kVP875Cut && kHPTG1Cut && kVPTG1Cut && kHPTG2Cut && kVPTG2Cut;
 
 } // stop using namespace ana
