189aeda3fe2882d7808c531869ad4ec162190e44
Joshua Berger <Joshua.Berger@colostate.edu>
2026-04-27 22:40:01 -0500

    Fixed minor but fatal bug in crossing weighting determination.

--- sbnana/SBNAna/icarus-analysis-villiage/pyana/dimuon-tools/nb/data.py
+++ sbnana/SBNAna/icarus-analysis-villiage/pyana/dimuon-tools/nb/data.py
@@ -80,7 +80,7 @@ def mc_dataset(f, key, hdrkey="hdr", mcnukey="mcnuwgt", syst_weights=True, mccut
     fluxcorr_wgt.name = ("wgt", "cv", "flux_other_than_ppfx", "", "", "") # JD 6/12/25
     df = add_weights(df, pd.DataFrame(fluxcorr_wgt)) # JD 6/12/25
     # JB 04/27/26: Adding calculation of track splitting reweighting here
-    cathode_cross_weight, cathode_cross_weight_err, gap_cross_weight, gap_cross_weight_err = track_splitting.correct(evtdf)
+    cathode_cross_weight, cathode_cross_weight_err, gap_cross_weight, gap_cross_weight_err = track_splitting.correct(df)
     cathode_cross_weight.name = ("wgt", "cv", "cathode_crossing", "", "", "")
     gap_cross_weight.name = ("wgt", "cv", "z_crossing", "", "", "")
     df = add_weights(df, pd.DataFrame(cathode_cross_weight))
